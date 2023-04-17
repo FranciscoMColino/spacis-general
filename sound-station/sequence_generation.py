10b47a4b66ed56f27da24a49d7094e17125e34e7
Francisco Colino <francisco.m.colino@gmail.com>
2023-04-17 14:44:45 +0100

    Add transform sequence function

--- sound-station/sequence_generation.py
+++ sound-station/sequence_generation.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -20,3 +22,28 @@ def preliminary_pseudorandom_binary_sequence(polynomial_array, generator_resolut
         array_place = i//32
         element_place = i%32
         result[array_place] |= (prbs_res << element_place)
+
+    return result
+
+def transform_sequence(preliminary_sequence, sequence_resolution):
+    sequence_size = pow(2, sequence_resolution) - 1
+    ps_size = math.ceil(sequence_size/32)    
+    
+    acc = 0
+    array_place = 0
+    element_place = 0
+    bit = 0
+    new_bit = 0
+
+    result = np.zeros(ps_size)
+
+    for i in range(0, sequence_size):
+        array_place = i//32
+        element_place = i%32
+        bit = ((preliminary_sequence[array_place] >> element_place) & 1) << element_place
+        acc += 3 if bit else 1
+        new_bit = (acc/16) % 2
+        result[array_place] &= ~(1 << element_place)
+        result[array_place] |= (new_bit << element_place)
+
+    return result
