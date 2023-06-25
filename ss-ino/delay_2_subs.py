3e9f0045869186c9c1cb81414d775abc51f2ff2b
Francisco Colino <francisco.m.colino@gmail.com>
2023-06-25 11:36:03 +0100

    Add ino code for transmitter and delay calculation code

---
+++ ss-ino/delay_2_subs.py
@@ -0,0 +1,24 @@
+import sys
+
+import numpy as np
+
+if __name__ == "__main__":
+    # first arg is subwoofer distance, second is number of delayed cycles
+    if len(sys.argv) < 3:
+        print("Usage: python delay_2_subs.py <sub_distance> <delay_cycles>")
+        sys.exit()
+
+    sub_distance = float(sys.argv[1])
+    delay_cycles = float(sys.argv[2])
+
+    # speed of sound in air
+    c = 343.0
+    delay_between_tx = 1/1600 # in seconds
+    distance_traveled = c * delay_between_tx * delay_cycles
+
+    print("Distance traveled: {} meters".format(distance_traveled))
+
+    transmission_angle = np.rad2deg(np.pi/2-np.arctan(distance_traveled/sub_distance))
+
+    print("Transmission angle: {} degrees".format(transmission_angle))
+    
