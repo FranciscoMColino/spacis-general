229c56346129ae7853a7555763b70f1eec6f58b3
Francisco Colino <francisco.m.colino@gmail.com>
2023-09-20 08:19:54 +0100

    Add default settings for subwoofers of sound station

--- sound-station/delay_module.py
+++ sound-station/delay_module.py
@@ -3,10 +3,12 @@ import tkinter as tk
 
 import navpy
 import numpy as np
+from gps_steering import calculate_ned_positions
 
 UPDATE_DELAYS_WAIT_TIME = 1/2
 
 DELAY_OFFSET = 25
+
 
 class DelayModule:
     def __init__(self, settings):
@@ -37,11 +39,11 @@ class DelayModule:
         self.subwoofer_array = {
             "sub0": {
                 "raw_pos_data": {
-                    "orientation": 0, #degrees
+                    "orientation": 0,  # degrees
                     "orientation_tk": tk.DoubleVar(),
-                    "height": 0, #meters
+                    "height": 0,  # meters
                     "height_tk": tk.DoubleVar(),
-                    "distance": 0, #meters
+                    "distance": 0,  # meters
                     "distance_tk": tk.DoubleVar(),
                 },
                 "ned_pos": [0, 0, 0],
@@ -50,11 +52,11 @@ class DelayModule:
             },
             "sub1": {
                 "raw_pos_data": {
-                    "orientation": 0, #degrees
+                    "orientation": settings["sub1_orientation"],  # degrees
                     "orientation_tk": tk.DoubleVar(),
-                    "height": 0, #meters
+                    "height": settings["sub1_height"],  # meters
                     "height_tk": tk.DoubleVar(),
-                    "distance": 0, #meters
+                    "distance": settings["sub1_distance"],  # meters
                     "distance_tk": tk.DoubleVar(),
                 },
                 "ned_pos": [0, 0, 0],
@@ -63,11 +65,11 @@ class DelayModule:
             },
             "sub2": {
                 "raw_pos_data": {
-                    "orientation": 0, #degrees
+                    "orientation": settings["sub2_orientation"],  # degrees
                     "orientation_tk": tk.DoubleVar(),
-                    "height": 0, #meters
+                    "height": settings["sub2_height"],  # meters
                     "height_tk": tk.DoubleVar(),
-                    "distance": 0, #meters
+                    "distance": settings["sub2_distance"],  # meters
                     "distance_tk": tk.DoubleVar(),
                 },
                 "ned_pos": [0, 0, 0],
@@ -76,11 +78,11 @@ class DelayModule:
             },
             "sub3": {
                 "raw_pos_data": {
-                    "orientation": 0, #degrees
+                    "orientation": settings["sub3_orientation"],  # degrees
                     "orientation_tk": tk.DoubleVar(),
-                    "height": 0, #meters
+                    "height": settings["sub3_height"],  # meters
                     "height_tk": tk.DoubleVar(),
-                    "distance": 0, #meters
+                    "distance": settings["sub3_distance"],  # meters
                     "distance_tk": tk.DoubleVar(),
                 },
                 "ned_pos": [0, 0, 0],
@@ -89,11 +91,11 @@ class DelayModule:
             },
             "sub4": {
                 "raw_pos_data": {
-                    "orientation": 0, #degrees
+                    "orientation": settings["sub4_orientation"],  # degrees
                     "orientation_tk": tk.DoubleVar(),
-                    "height": 0, #meters
+                    "height": settings["sub4_height"],  # meters
                     "height_tk": tk.DoubleVar(),
-                    "distance": 0, #meters
+                    "distance": settings["sub4_distance"],  # meters
                     "distance_tk": tk.DoubleVar(),
                 },
                 "ned_pos": [0, 0, 0],
@@ -102,11 +104,11 @@ class DelayModule:
             },
             "sub5": {
                 "raw_pos_data": {
-                    "orientation": 0, #degrees
+                    "orientation": settings["sub5_orientation"],  # degrees
                     "orientation_tk": tk.DoubleVar(),
-                    "height": 0, #meters
+                    "height": settings["sub5_height"],  # meters
                     "height_tk": tk.DoubleVar(),
-                    "distance": 0, #meters
+                    "distance": settings["sub5_distance"],  # meters
                     "distance_tk": tk.DoubleVar(),
                 },
                 "ned_pos": [0, 0, 0],
@@ -116,10 +118,11 @@ class DelayModule:
         }
 
         self.update_azi_ele()
+        calculate_ned_positions(self.subwoofer_array)
 
     async def calculate_delays(self):
 
-        SOUND_SPEED = 343 #m/s
+        SOUND_SPEED = 343  # m/s
         TRANSMISSION_FREQ = 1600
 
         while True:
@@ -127,15 +130,18 @@ class DelayModule:
             if self.manual_delays_var.get():
                 await asyncio.sleep(UPDATE_DELAYS_WAIT_TIME)
                 continue
-            
-            balloon_lat, balloon_lon, balloon_alt = self.balloon_lla_pos["lat"], self.balloon_lla_pos["lon"], self.balloon_lla_pos["alt"]
-            tx_center_lat, tx_center_lon, tx_center_alt = self.subarray_lla_pos["lat"], self.subarray_lla_pos["lon"], self.subarray_lla_pos["alt"]
+
+            balloon_lat, balloon_lon, balloon_alt = self.balloon_lla_pos[
+                "lat"], self.balloon_lla_pos["lon"], self.balloon_lla_pos["alt"]
+            tx_center_lat, tx_center_lon, tx_center_alt = self.subarray_lla_pos[
+                "lat"], self.subarray_lla_pos["lon"], self.subarray_lla_pos["alt"]
 
             if balloon_lat == tx_center_lat and balloon_lon == tx_center_lon and balloon_alt == tx_center_alt:
                 await asyncio.sleep(UPDATE_DELAYS_WAIT_TIME)
                 continue
 
-            balloon_ned = navpy.lla2ned(balloon_lat, balloon_lon, balloon_alt, tx_center_lat, tx_center_lon, tx_center_alt)
+            balloon_ned = navpy.lla2ned(
+                balloon_lat, balloon_lon, balloon_alt, tx_center_lat, tx_center_lon, tx_center_alt)
 
             direction_versor = balloon_ned / np.linalg.norm(balloon_ned)
 
@@ -143,10 +149,12 @@ class DelayModule:
 
             delays[0] = DELAY_OFFSET
 
-            for i in range(1,6):
-                sub_ned_pos = self.subwoofer_array["sub{}".format(i)]["ned_pos"]
+            for i in range(1, 6):
+                sub_ned_pos = self.subwoofer_array["sub{}".format(
+                    i)]["ned_pos"]
                 delay_dist = np.dot(sub_ned_pos, direction_versor)
-                delay_time = round((delay_dist / SOUND_SPEED) / (1/TRANSMISSION_FREQ))
+                delay_time = round(
+                    (delay_dist / SOUND_SPEED) / (1/TRANSMISSION_FREQ))
                 delays[i] = delay_time + DELAY_OFFSET
 
             delay_min = min(delays)
@@ -157,21 +165,22 @@ class DelayModule:
                 self.subwoofer_array["sub{}".format(i)]["delay"] = delays[i]
 
             await asyncio.sleep(UPDATE_DELAYS_WAIT_TIME)
-    
+
     def update_delays(self, *args):
-        
+
         if not self.manual_delays_var.get():
             return
 
         for i in range(6):
-            self.subwoofer_array["sub{}".format(i)]["delay"] = self.subwoofer_array["sub{}".format(i)]["delay_tk"].get()
+            self.subwoofer_array["sub{}".format(
+                i)]["delay"] = self.subwoofer_array["sub{}".format(i)]["delay_tk"].get()
 
     def update_target(self):
         if self.manual_target_var.get():
             self.balloon_lla_pos["lat"] = self.balloon_lla_pos["lat_tk"].get()
             self.balloon_lla_pos["lon"] = self.balloon_lla_pos["lon_tk"].get()
             self.balloon_lla_pos["alt"] = self.balloon_lla_pos["alt_tk"].get()
-        
+
         self.update_azi_ele()
 
     def update_raw_pos_data(self):
@@ -179,8 +188,8 @@ class DelayModule:
         self.subarray_lla_pos["lat"] = self.subarray_lla_pos["lat_tk"].get()
         self.subarray_lla_pos["lon"] = self.subarray_lla_pos["lon_tk"].get()
         self.subarray_lla_pos["alt"] = self.subarray_lla_pos["alt_tk"].get()
-        
-        for i in range(1,6):
+
+        for i in range(1, 6):
             sub = self.subwoofer_array["sub{}".format(i)]
             sub["raw_pos_data"]["orientation"] = sub["raw_pos_data"]["orientation_tk"].get()
             sub["raw_pos_data"]["height"] = sub["raw_pos_data"]["height_tk"].get()
@@ -192,14 +201,12 @@ class DelayModule:
 
         fi1 = np.radians(self.subarray_lla_pos["lat"])
         fi2 = np.radians(self.balloon_lla_pos["lat"])
-        delta_lambda = np.radians(self.balloon_lla_pos["lon"] - self.subarray_lla_pos["lon"])
+        delta_lambda = np.radians(
+            self.balloon_lla_pos["lon"] - self.subarray_lla_pos["lon"])
         x = np.cos(fi2) * np.sin(delta_lambda)
-        y = np.cos(fi1) * np.sin(fi2) - np.sin(fi1) * np.cos(fi2) * np.cos(delta_lambda)
+        y = np.cos(fi1) * np.sin(fi2) - np.sin(fi1) * \
+            np.cos(fi2) * np.cos(delta_lambda)
         azimuth = np.arctan2(x, y)
-
 
         self.azimuth = ((azimuth * 180 / np.pi) + 360) % 360
         self.elevation = 0
-
-
-        
